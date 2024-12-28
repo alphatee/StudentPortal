28493eccc4971dbda8acc2960e0467fd31ffa946
Daniel Herrera <dher426@wgu.edu>
2024-12-28 10:52:56 -0800

    60% through tutorial

---
+++ StudentPortal.Web/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentPortal.Web.Models;
+
+namespace StudentPortal.Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddStudentViewModel viewModel)
+        {
+            // 39 min 19 seconds 
+            return View();
+        }
+    }
+}
