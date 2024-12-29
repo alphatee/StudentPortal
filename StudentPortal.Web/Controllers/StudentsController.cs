fb4ef677ab68f95a8cb464e8a071f773038d9d40
Daniel Herrera <dher426@wgu.edu>
2024-12-28 22:55:56 -0800

    finished tutorial

--- StudentPortal.Web/Controllers/StudentsController.cs
+++ StudentPortal.Web/Controllers/StudentsController.cs
@@ -1,10 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Data;
 using StudentPortal.Web.Models;
+using StudentPortal.Web.Models.Entities;
 
 namespace StudentPortal.Web.Controllers
 {
     public class StudentsController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
@@ -12,10 +22,70 @@ namespace StudentPortal.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Add(AddStudentViewModel viewModel)
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
         {
-            // 39 min 19 seconds 
+            var student = new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed
+            };
+
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var students = await dbContext.Students.ToListAsync();
+
+            return View(students);  
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var student = await dbContext.Students.FindAsync(id);
+
+            return View(student); 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student viewModel)
+        {
+            var student = await dbContext.Students.FindAsync(viewModel.Id);
+
+            if (student is not null)
+            {
+                student.Name = viewModel.Name;
+                student.Email = viewModel.Email;
+                student.Phone = viewModel.Phone;
+                student.Subscribed = viewModel.Subscribed;
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Students"); // 2nd arg is the name of controller. which i am passing back the same controller this post request is in
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student viewModel)
+        {
+            var student = await dbContext.Students
+                .AsNoTracking() 
+                .FirstOrDefaultAsync(x => x.Id == viewModel.Id);
+
+            if(student is not null)
+            {
+                dbContext.Students.Remove(student);
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Students");
         }
     }
 }
